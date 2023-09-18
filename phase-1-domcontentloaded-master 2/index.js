af14d0e16efea3165de11dceefcf3fe00ae147ee
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-18 17:30:08 -0400

    phase-1-domcontentloaded/
    phase-1-domcontentloaded/

--- phase-1-domcontentloaded-master 2/index.js
+++ phase-1-domcontentloaded-master 2/index.js
@@ -1,19 +1,27 @@
-index.js;
-javascript
-// No code here for now
+index.html:
+html
+<!DOCTYPE html>
+<html>
+<body>
+<p id="text">JavaScript is so cool. It lets me add text to my page programmatically.</p>
+<script src="index.js"></script>
+</body>
+</html>
 
-ndex.js;
+index.js:
 javascript
-// No code here for now;
+document.addEventListener('DOMContentLoaded', function() {
+const paragraph = document.getElementById('text');
+paragraph.textContent = 'This is really cool!';
+});
 
-indexTest.js;
+indexTest.js:
 ```javascript
 const { expect } = require('chai');
 
 describe('DOM manipulation', function() {
 it('should change the text on the page when DOM content has loaded', function() {
 const paragraph = document.getElementById('text');
-paragraph.textContent = 'This is really cool!';
 expect(paragraph.textContent).to.equal('This is really cool!');
 });
 });
