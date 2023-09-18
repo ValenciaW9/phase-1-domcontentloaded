9a4a4b3d1429631a17c0fc17f12a1cc069f2d54c
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-18 17:16:21 -0400

    phase-1-domcontentloaded/
    phase-1-domcontentloaded/

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    <p id="text">This is really cool!</p>
-  </body>
-</html>
 document.addEventListener('DOMContentLoaded', function() {
-  const paragraph = document.getElementById('text');
-  paragraph.textContent = 'This is really cool!';
-});
+    const paragraph = document.getElementById('text');
+    paragraph.textContent = 'This is really cool!';
+  });
+
+<script src="index.js"></script>
