d58abae50b3e6be42068cc1e6bf42661096e351c
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-18 17:26:52 -0400

    phase-1-domcontentloaded/
    phase-1-domcontentloaded/

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const paragraph = document.getElementById('text');
-    paragraph.textContent = 'This is really cool!';
-  });
+index.js;
+javascript
+// No code here for now
 
-<script src="index.js"></script>
+ndex.js;
+javascript
+// No code here for now;
+
+indexTest.js;
+```javascript
+const { expect } = require('chai');
+
+describe('DOM manipulation', function() {
+it('should change the text on the page when DOM content has loaded', function() {
+const paragraph = document.getElementById('text');
+paragraph.textContent = 'This is really cool!';
+expect(paragraph.textContent).to.equal('This is really cool!');
+});
+});
+```
