504e4c54864b78b5a9f77fc3f9a463be83ce52d0
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-18 16:53:56 -0400

    phase-1-domcontentloaded/
    phase-1-domcontentloaded/

--- phase-1-domcontentloaded-master 2/index.js
+++ phase-1-domcontentloaded-master 2/index.js
@@ -1,7 +1,12 @@
 index.html
 ```html
 <!DOCTYPE html>
+expected 'JavaScript is so cool. It lets me add…' to equal 'This is really cool!'
 
+      + expected - actual
+
+      -JavaScript is so cool. It lets me add text to my page programmatically.
+      +This is really cool!
 
 
 
@@ -17,5 +22,3 @@ script.js
 javascript
 document.addEventListener("DOMContentLoaded", function() {
 const paragraph = document.getElementById("text");
-paragraph.textContent = "This is really cool!";
-});
